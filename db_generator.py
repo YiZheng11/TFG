eca8ba7b391837291ba5390122181f4c1ccc65af
YiZheng11 <157117876+YiZheng11@users.noreply.github.com>
2024-03-05 16:36:30 +0100

    More additions to the code because of consecutive Internal Server Error from NCBI
    Additions:
    - API KEY to increase the requests from 3 to 10 requests per second
    - time.sleep(0.15) after Entrez utilities
    - Code to skip a download of a file already existing in the directory /data
    - Adding a retry logic when there is a Internal Server Error with a wait of 60 seconds before it retries again

--- db_generator.py
+++ db_generator.py
@@ -3,19 +3,20 @@ import urllib
 import gzip
 import pdb
 import time
+from urllib.error import HTTPError 
 #import subprocess
 
 from pathlib import Path
 
 from Bio import Entrez
-#from Bio import SeqIO
 
-Entrez.email = "yi.zheng@alumnos.upm.es" # your email please
-Entrez.api_key = "1c105008de567a0fdcc74eedb9584b2ec109"  # your API key
+Entrez.email = "YOUR_EMAIL" # your email please
+Entrez.api_key = "YOUR_API_KEY"  # your API key
 
 def get_index(number):
     "Return a NCBI summary of the assembly with id <number>"
     handle = Entrez.esummary(db="assembly", id=number, report="full")
+    time.sleep(0.15)
     return Entrez.read(handle)
 
 def get_url(index):
@@ -35,6 +36,7 @@ def get_assemblies(term, target_dir, retmax=0):
     handle = Entrez.esearch(db="assembly", term=term, retmax=retmax)
     record = Entrez.read(handle)
     print(f"Found {len(record['IdList'])} records matching {term}")
+    time.sleep(0.15)
 
     for number in record["IdList"]:
         index = get_index(number)
@@ -47,15 +49,39 @@ def get_assemblies(term, target_dir, retmax=0):
         
         name = Path(url).name
         files = [f"{url}/{name}_genomic.fna.gz", f"{url}/{name}_genomic.gbff.gz"]
-        for filename in files:
-            print(f"Fetching {filename} from NCBI")
-            with urllib.request.urlopen(filename) as response:
-                with open(f"{target_dir}/{Path(filename).name}", "wb") as outfile:
-                    outfile.write(response.read())
-        
-        time.sleep(0.1)  # Sleep for 10 requests per second aprox
+        for file_url in files: 
+            filename = Path(file_url).name
+            filepath = Path(target_dir) / filename
+
+            if not filepath.exists():
+                print(f"Downloading {filename} from NCBI")
+                with urllib.request.urlopen(url) as response:
+                    with open(filepath, "wb") as outfile:
+                        outfile.write(response.read())
+            else: # In case some of the assemblies have already been downloaded
+                print(f"Skipping {filename}, already exists")
 
     return None
+
+def get_assemblies_with_retry(term, target_dir, retmax=0, max_retries=20):
+    "Download to <target_dir> all fasta and genbank for assemblies matching <term>, with retry"
+    retries = 0
+    success = False
+
+    while not success and retries < max_retries:
+        try:
+            get_assemblies(term, target_dir, retmax)
+            success = True
+        except HTTPError as e:
+            if e.code == 500:  # Internal Server Error
+                print("Internal Server Error, retrying again in 60 seconds...")
+                time.sleep(60)
+                retries += 1
+            else:
+                raise  # If it is not an Internal Server Error, it shows the exception/error captured
+
+    if not success:
+        print(f"Failed to download the assemblies for {term} after {max_retries} retries.")
 
 def unzip_all(base_dir):
     "Use gunzip to decompress all gzipped files under <base_dir>"
@@ -75,6 +101,11 @@ def directory_to_database(directory, title):
     directory = Path(directory)
     fastas = " ".join(map(str, directory.rglob("*.fna")))
     
+    #if fastas:
+    #    command = f"makeblastdb -in '{fastas}' -title {title} -out {directory}/{title} -parse_seqids -dbtype nucl"
+    #    if command is not None:
+    #        subprocess.Popen(command, shell=True).wait()
+    
     if fastas:
         os.system(f"makeblastdb -in '{fastas}' -title {title} -out {directory}/{title} -parse_seqids -dbtype nucl")
     return None
@@ -87,6 +118,8 @@ if __name__ == "__main__":
                  'Sinorhizobium', 'Neorhizobium', 'Georhizobium', 
                  'Pararhizobium', 'Pseudorhizobium']
     
-    get_assemblies('Rhizobium', data_dir, retmax=100000)
+    for organism in organisms:
+        get_assemblies_with_retry(organism, data_dir, retmax=100000)
+    
     unzip_all(data_dir)
-    directory_to_database(data_dir, "bradyrhizobia_blastdb")
+    directory_to_database(data_dir, "rizobia_blastdb")
