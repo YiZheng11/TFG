21bbadb7f9793ce4d11f5a9f211841a3914b9b8d
YiZheng11 <157117876+YiZheng11@users.noreply.github.com>
2024-03-18 15:02:52 +0100

    Filtering of XML file using the minimumIdentity, minimumCoverage and maximumEValue

--- blastanalysis.py
+++ blastanalysis.py
@@ -2,8 +2,7 @@ import os
 import subprocess
 
 def blast_data(query, db_name, output_path):
-    command = f"tblastn -query {query} -db {db_name} -outfmt 5 -max_target_seqs '2000' -out {output_path}.xml"
-    #command = f"tblastn -query {query} -db {db_name} -outfmt '6 qseqid sseqid pident qcovs evalue sseq' -max_target_seqs '2000' -out {output_path}.tsv"
+    command = f"tblastn -query {query} -db {db_name} -outfmt 5 -max_target_seqs '2000' -out {output_path}"
     
     if command is not None:
         subprocess.Popen(command, shell=True).wait()
@@ -11,17 +10,17 @@ def blast_data(query, db_name, output_path):
     else:
         print("Error, tblastn couldn't be done, please check.")
 
-#def filter_blast(blast_data, output_filtered, minimumIdentity, minimumCoverage, maximumEValue):
-#    with open(blast_data, 'r') as input_file, open(output_filtered, 'w') as output_file:
-#        for line in input_file:
-#            columns = line.split()
-#            identity = float(columns[3])
-#            coverage = float(columns[4])
-#            evalue = float(columns[5])
-                
-#            if identity >= minimumIdentity and coverage >= minimumCoverage and evalue <= maximumEValue:
-#                output_file.write(line)
-
+def filter_blast(blast_xml, output_file, minimumIdentity, minimumCoverage, maximumEValue):
+    with open(output_file, 'w') as out_handle:
+        out_handle.write("query_id\thit_id\tpident\tqcovs\tevalue\n")
+        result_handle = open(blast_xml)
+        blast_records = NCBIXML.parse(result_handle)
+        for blast_record in blast_records:
+            for alignment in blast_record.alignments:
+                for hsp in alignment.hsps:
+                    if hsp.expect <= maximumEValue and hsp.align_length / blast_record.query_length * 100 >= minimumCoverage and hsp.identities / hsp.align_length * 100 >= minimumIdentity:
+                        out_handle.write(f"{blast_record.query_def}\t{alignment.hit_id}\t{hsp.identities / hsp.align_length * 100:.2f}\t{hsp.align_length / blast_record.query_length * 100:.2f}\t{hsp.expect}\n")
+        result_handle.close()
 
 if __name__ == "__main__":
     query = f"{os.getcwd()}/T6SS_genes_2.fasta"
@@ -29,12 +28,12 @@ if __name__ == "__main__":
     
     output_dir = "rizobia-blast"
     os.makedirs(output_dir, exist_ok=True)
-    output = f"{os.getcwd()}/{output_dir}/tblastnResult_7"
-#    output_filtered = "tblastnResult_Filtered_7"
+    output = f"{os.getcwd()}/{output_dir}/tblastnResult_T6SS_I.xml"
+    output_filtered = "tblastnResult_T6SS_I_Filtered"
     
     #minimumIdentity = 30
     #minimumCoverage = 50
     #maximumEValue = 1e-5
     
     blast_data(query, db_name, output)
-#    filter_blast(output, output_filtered, minimumIdentity, minimumCoverage, maximumEValue)
+    filter_blast(output, output_filtered, minimumIdentity, minimumCoverage, maximumEValue)
