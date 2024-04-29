0404bf330620000ee5cd4777d58981558c7f5b19
YiZheng11 <157117876+YiZheng11@users.noreply.github.com>
2024-04-29 10:10:49 +0200

    Update rpsblast.py

--- rpsblast.py
+++ rpsblast.py
@@ -1,58 +1,66 @@
 import os
+import io
 import subprocess
-import tempfile
+from subprocess import PIPE
 
-from Bio import SeqIO
+from Bio.SeqIO import parse
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
 from pathlib import Path
+from Bio import SearchIO
 
 def profile_db(PSSM_file, output):
     "Create profile db with the PSSMs named in <PSSM_file> and saved it in <output>"
     command = f"makeprofiledb -in {PSSM_file} -out {output}"
+    subprocess.Popen(command, shell=True).wait()
+    print(f"Check out {output} where the db is saved.")
 
-    try:
-        subprocess.Popen(command, shell=True).wait()
-        print(f"Check out {output} where the db is saved.")
-    except:
-        print("Error, makeprofiledb couldn't be done, please check.")
+def cds_feature_to_record(cds):
+    seq = Seq(cds.qualifiers.get("translation", [""])[0])
+    protein_id = cds.qualifiers.get("protein_id", [""])[0]
+    locus_tag = cds.qualifiers.get("locus_tag", [""])[0]
+    return SeqRecord(seq, id=locus_tag, description=protein_id)
 
-def create_multifasta(directory, genus):
-    "Create a temporary multifasta file from all translated sequences in the directory."
-    #temp_multifasta = tempfile.NamedTemporaryFile(delete=False)
-    gb_files = directory.rglob("*.gbff")
-    temp_multifasta = f"temp_multifasta_{genus}"
-
-    with open(temp_multifasta, "w") as multifasta_file:
-        for gb_file in gb_files:
-            for record in SeqIO.parse(gb_file, "genbank"):
-                if genus in record.description:
-                    locus_id = record.id
-                    for feature in record.features:
-                        if feature.type == "CDS":
-                            protein_id = feature.qualifiers.get("protein_id", ["Unknown"])[0]
-                            translation = feature.qualifiers.get("translation", [""])[0]
-                            if translation:
-                                multifasta_file.write(f">{locus_id} {protein_id}\n{translation}\n")
-                            else:
-                                try:
-                                    location = feature.location
-                                    nucleotide_seq = location.extract(record).seq
-                                    translation = nucleotide_seq.translate()
-                                    multifasta_file.write(f">{locus_id} {protein_id}\n{translation}\n")
-                                except:
-                                    multifasta_file.write(f">{locus_id} {protein_id}\n{translation}\n")
-    return None # Need to change it so it returns the name of the temp file
+def fasta_generator(directory, genus):
+    for gb_file in Path(directory).rglob("*.gbff"):
+        print(f"For {gb_file.name}")
+        for record in parse(gb_file, "genbank"):
+            multifasta = ""
+            for cds in filter(lambda x: x.type == "CDS", record.features):
+                protein = cds_feature_to_record(cds)
+                if len(protein.seq) > 0:
+                    multifasta += protein.format("fasta")
+            yield multifasta
+    return None
 
 def rpsblast(directory, profile_db, output_name, genus):
     "Perform RPS BLAST using sequences extracted from GenBank files"
     directory = Path(directory)
-    temp_multifasta_file = create_multifasta(directory, genus)
-
-    command = ["rpsblast", "-query", f"temp_multifasta_{genus}", "-db", profile_db, "-out", output_name, "-outfmt", "5", "-max_target_seqs", "8000", "-evalue", "0.00001"]
+    #temp_multifasta_file = create_multifasta(directory, genus)
     
-    process = process = subprocess.run(command)
-
-    # Remove the temporary multifasta file
-    #Path("tmpebuzvxfx").unlink()
+    command = ["rpsblast"]
+    command += ["-query", "-"]
+    command += ["-db", profile_db]
+    #command += ["-out", output_name]
+    command += ["-outfmt", "5"]
+    #command += ["max_target_seqs", "8000"]
+    command += ["-evalue", "0.00001"]
+    command += ["-max_hsps", "1"]
+    
+    queries = 0
+    hits = 0
+    qresults = []
+    for fasta in fasta_generator(directory, genus):
+        if fasta:
+            process = subprocess.Popen(command, text=True, stdin=PIPE, stdout=PIPE)
+            xmlout, _ = process.communicate(input=fasta)
+            for qresult in SearchIO.parse(io.StringIO(xmlout), "blast-xml"):
+                queries += 1
+                if len(qresult.hits) > 0:
+                    hits += len(qresult.hits)
+                    qresults += [qresult]
+        print(f"nQueries: {queries} nHits: {hits}", end="\r")
+    SearchIO.write(qresults, "rpsblast_DUF1795_Rhizobium.xml", "blast-xml")
     return None
 
 if __name__ == "__main__":
@@ -62,7 +70,7 @@ if __name__ == "__main__":
     output_name_profile = "DUF1795_profiledb"
 
     dir_protein = os.path.join(os.getcwd(), "rizobia-data")
-    output_rps = os.path.join(os.getcwd(), "rizobia-blast", "rpsblastResult_DUF1795_Mesorhizobium.xml")
+    output_rps = os.path.join(os.getcwd(), "rizobia-blast", "rpsblastResult_DUF1795_Lewis.xml")
 
-    profile_db(PSSM_file, os.path.join(output_dir_profile, output_name_profile))
-    rpsblast(dir_protein, f"{os.getcwd()}/rizobia-profiledb/DUF1795_profiledb", output_rps, "Mesorhizobium")
+    #profile_db(PSSM_file, os.path.join(output_dir_profile, output_name_profile))
+    rpsblast(dir_protein, f"{os.getcwd()}/rizobia-profiledb/DUF1795_profiledb", output_rps, "Rhizobium")
