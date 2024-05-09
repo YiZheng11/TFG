936264ae0a7203914618be0974eea915c4a42a71
YiZheng11 <157117876+YiZheng11@users.noreply.github.com>
2024-05-09 15:52:31 +0200

    Update count_organisms.py

--- count_organisms.py
+++ count_organisms.py
@@ -2,9 +2,8 @@ import os
 import pickle
 
 from pathlib import Path
-from Bio.Blast import NCBIXML
 from Bio.SeqIO import parse
-from Bio import SeqIO
+from Bio import SeqIO, SearchIO
 
 class Accession:
     def __init__(self, accession, path_filename):
@@ -57,12 +56,45 @@ def create_objs(directory):
     with open('accessions.pkl', 'wb') as file:
             pickle.dump(objs, file)
 
+def load_pickle_file(pickle_file):
+    with open(f"{pickle_file}", 'rb') as file:
+        loaded_accessions = pickle.load(file)
+    return loaded_accessions
+
+def parse_blast(blast_xml, pickle_file):
+    nodC = set()
+    i=0
+    qresults = SearchIO.parse(f"{blast_xml}", "blast-xml")
+    for qresult in qresults:
+        for hit in qresult.hits:
+            for obj in objs:
+                for locus_id in obj.loci:
+                    if hit.accession in locus_id:
+                        print(hit.accession, locus_id, obj.name)
+                        i += 1
+                        print(f"nHits: {i}", end="\r")
+                        obj.nodC = True
+                        nodC.add(obj.name)
+                        
+    with open(f"{pickle_file}", 'wb') as file:
+        pickle.dump(objs, file)
+    
+    with open("nodC_species", "w") as nodC_file:
+        for organism in nodC:
+            nodC_file.write(organism+"\n")
+
 if __name__ == "__main__":
-    nodABC_file = f"{os.getcwd()}/rizobia-blast/tblastnResult_nodABC.xml"
-    nifH_file = f"{os.getcwd()}/rizobia-blast/tblastnResult_nifH.xml"
-    T6SS_file = f"{os.getcwd()}/rizobia-blast/tblastnResult_T6SS.xml"
+    rizobia_blast = os.path.join(os.getcwd(), "rizobia-blast")
+    nodA_file = os.path.join(rizobia_blast, "tblastnResult_nodA.xml")
+    nodB_file = os.path.join(rizobia_blast, "tblastnResult_nodB.xml")
+    nodC_file = os.path.join(rizobia_blast, "tblastnResult_nodC.xml")
+    nifH_file = os.path.join(rizobia_blast, "tblastnResult_nifH.xml")
+    T6SS_file = os.path.join(rizobia_blast, "tblastnResult_T6SS.xml")
 
     data_dir = os.path.join(os.getcwd(), "rizobia-data")
+    pickle_file = os.path.join(os.getcwd(), "accessions.pkl")
     
     genera = ["Rhizobium", "Bradyrhizobium", "Mesorhizobium", "Sinorhizobium"]
-    create_objs(data_dir)
+    #create_objs(data_dir, pickle_file)
+    objs = load_pickle_file(pickle_file)
+    parse_blast(nodC_file, pickle_file)
