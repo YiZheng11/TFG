8bcb64308bfee8e09bd0cd16b1b516ff7ddb185e
YiZheng11 <157117876+YiZheng11@users.noreply.github.com>
2024-06-14 12:02:46 +0200

    Update adaptor_in_cluster.py

--- adaptor_in_cluster.py
+++ adaptor_in_cluster.py
@@ -1,5 +1,6 @@
 import os
 import re
+import pdb
 
 def parse_clusters(filename):
     with open(filename, 'r') as file:
@@ -19,7 +20,6 @@ def parse_clusters(filename):
                 checking_cluster.append(line)
 
     clusters.append(checking_cluster)
-
     return clusters
 
 def check_BLAST_hits_in_cluster(cluster):
@@ -29,30 +29,52 @@ def check_BLAST_hits_in_cluster(cluster):
     for line in cluster:
         match = pattern.search(line)
         if match:
-            print(line.strip())
             nHits = int(match.group(1))
             break
-
     return nHits
+
+def check_location(cluster, adaptor_start, adaptor_end):
+    cluster_start = None
+    cluster_end = None
+    cluster_lines = []
+    found = False
+    
+    for line in cluster:
+        if not found:
+            if line.startswith("Table of genes, locations, strands and annotations of subject cluster:"):
+                found = True
+                continue
+        
+        if found:
+            if line.startswith("\n"):
+                break
+            cluster_lines.append(line)
+            
+    cluster_start = int(cluster_lines[0].split("\t")[1])
+    cluster_end = int(cluster_lines[-1].split("\t")[2])
+    
+    if cluster_start <= adaptor_start <= cluster_end and cluster_start <= adaptor_end <= cluster_end:
+        return True
+    else:
+        return False
 
 def save_adaptor_info(file):
     all_adaptor = []
     with open(f"{file}", "r") as adaptor_file:
         adaptor_file.readline()
         for line in adaptor_file:
-            record = line.split("\t")[1]
-            start = line.split("\t")[2]
-            end = line.split("\t")[3]
-            strand = line.split("\t")[5]
-            hit = (record, start, end, strand)
-            all_adaptor.append(hit)
+            adaptor = line.strip().split("\t")
+            all_adaptor.append(adaptor)
     return all_adaptor
 
 if __name__ == "__main__":
+    output_file = os.path.join(os.getcwd(), "adaptor_not_in_cluster_results.tsv")
+    
     adaptor_file = os.path.join(os.getcwd(), "adjacent_genes_DUF4123.tsv")
     adaptor_info = save_adaptor_info(adaptor_file)
     all_records = set()
     in_cluster = set()
+    
     for i in range(18):
         n = i + 1
         directory = os.path.join(os.getcwd(), "rizobia_cluster", f"rizobia_cluster_{n}")
@@ -60,23 +82,23 @@ if __name__ == "__main__":
     
         clusters = parse_clusters(file)
         
-        # To look at the results
         for adaptor_hit in adaptor_info:
-            record = adaptor_hit[0]
-            start = adaptor_hit[1]
-            end = adaptor_hit[2]
-            strand = adaptor_hit[3]
+            record = adaptor_hit[1]
+            start = int(adaptor_hit[2])
+            end = int(adaptor_hit[3])
+            strand = adaptor_hit[5]
             all_records.add(record)
-            for i, cluster in enumerate(clusters):
+            for cluster in clusters:
                 index = cluster[2].find(" ")
                 cluster_record = (cluster[2]).strip()[index+1:-2]
                 if cluster_record in record:
-                    print(f"Cluster {n}:")
                     nHits = check_BLAST_hits_in_cluster(cluster)
-                    
-                    in_cluster.add(record)
+                    adaptor_in = check_location(cluster, start, end)
+                    if adaptor_in:
+                        in_cluster.add(record)
                 
     not_in_cluster = all_records - in_cluster
-    print(len(all_records))
-    print(len(in_cluster))
+    print(f"Total number of adaptors found initially (non repeated): {len(all_records)}")
+    print(f"Number of those that are in a cluster: {len(in_cluster)}")
+    print(f"Number of those that are NOT in a cluster: {len(not_in_cluster)}")
     print(not_in_cluster)
